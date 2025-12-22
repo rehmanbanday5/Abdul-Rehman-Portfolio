771d60d26f868827df99bdb27419c53f3fb04831
Rehman Banday <rehmanbanday5@gmail.com>
2025-12-22 21:09:42 +0500

    Day 4: Almost Done

---
+++ script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints:{
+    '480':{
+      slidesPerView: 1,
+    },
+    '800':{
+      slidesPerView: 2,
+    },
+    '1200':{
+      slidesPerView: 3,
+    }
+
+
+  }
+});
